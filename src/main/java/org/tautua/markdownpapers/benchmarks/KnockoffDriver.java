afd6e3268e0b213060dd341c406cf5db468b08c4
Larry Ruiz <larry.b.ruiz@gmail.com>
2011-08-05 10:29:52 -0500

    License header added

--- src/main/java/org/tautua/markdownpapers/benchmarks/KnockoffDriver.java
+++ src/main/java/org/tautua/markdownpapers/benchmarks/KnockoffDriver.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.tautua.markdownpapers.benchmarks;
 
 import com.sun.japex.JapexDriverBase;
@@ -9,13 +25,6 @@ import scala.xml.XML;
 import static com.tristanhunt.knockoff.DefaultDiscounter.*;
 import static org.tautua.markdownpapers.benchmarks.Util.slurp;
 
-/**
- * Created by IntelliJ IDEA.
- * User: lruiz
- * Date: 7/26/11
- * Time: 2:11 AM
- * To change this template use File | Settings | File Templates.
- */
 public class KnockoffDriver extends JapexDriverBase {
     private String content;
 
